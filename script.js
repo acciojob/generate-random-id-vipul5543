f84b4bb6e8d976a66ee942ce693e8fe667a010c6
vipul5543 <129362602+vipul5543@users.noreply.github.com>
2025-01-09 18:38:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function makeid(l) {
   // write your code here
+let characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let res = "";
+  for(let i = 0;i<l;i++){
+      const randomIndex = Math.floor(Math.random()*characters.length);
+      res = res + characters[randomIndex];
+  }
+	return res;	
 }
+
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
